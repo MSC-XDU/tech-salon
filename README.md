31d217030e4c2889386bafda3ebb83c1c1f06661
dlmyb <a@dlmyb.com>
2016-11-20 21:42:22 +0800

    upload file

---
+++ README.md
@@ -0,0 +1,6 @@
+### 2016.11.19 技术沙龙
+
+这次技术沙龙时间的冲突，所以把结果录制成了视频，大家可以点击下面的链接自行观看
+
+[上半场：Docker的入门和架构](https://v.qq.com/x/page/k0347c6z49e.html)
+[下半场：OOP的设计模式](https://v.qq.com/x/page/g03470ewf5g.html)
---
+++ docker/app.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+__author__ = 'calculusma'
+
+from flask import Flask,request,render_template,Response
+from docker import Client
+import time,datetime
+
+def init():
+    c = Client(base_url="unix://var/run/docker.sock")
+    # cid = c.create_container(
+    #             image="python:2.7.12-slim",
+    #             volumes='/codebase',
+    #             host_config=c.create_host_config(binds={
+    #                 os.getcwd()+'/tmp':{
+    #                     "bind":"/codebase",
+    #                     "mode":"ro"
+    #                     }
+    #                 }
+    #             ),
+    #             command="python /codebase/test.py"
+    #         )["Id"]
+    cid = "478dc814cb51"
+    return c,cid
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        code = request.form.get("code")
+        t = int(time.time())
+        with open("tmp/test.py", "w+") as f:
+            f.write(code)
+        c.start(cid)
+        result = c.logs(cid,
+                        since=t,
+                        stream=True,
+                        # timestamps=True
+        )
+        return Response(result,mimetype="text")
+    return render_template("index.html")
+
+
+@app.route("/test")
+def test():
+    def g():
+        for i in range(1000):
+            time.sleep(0.1)
+            yield str(i)+" "
+    sg = g()
+    return Response(sg, mimetype="text/event-stream")
+
+if __name__ == "__main__":
+    c, cid = init()
+    app.run(port=5000, debug=True)
